0bfea2c13a866fbd4b92980d01f64c5397ef1686
renato.paula <renato.paula@solucionarelj.com.br>
2020-07-30 21:20:52 -0300

    codificando  UseState

--- src/Pages/Cadastro/Categoria/index.js
+++ src/Pages/Cadastro/Categoria/index.js
@@ -1,11 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import PageDefault from "../../../components/PageDefault";
+import FormField from "../../../components/FormField";
 
 function CadastroCategoria() {
+  const valoresIniciais = {
+    nome: "",
+    descricao: "",
+    cor: "",
+  };
+
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
+  }
+
+  function handleChange(e) {
+    setValue(e.target.getAtrribute("name"), e.target.value);
+  }
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {values.nome}</h1>
+
+      <form
+        onSubmit={function handleSubmit(e) {
+          e.preventDefault();
+
+          setCategorias([...categorias, values]);
+        }}
+      >
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Descrição"
+          type="????"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+
+        <button>Cadastrar</button>
+      </form>
+
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return <li key={`${categoria}${indice}`}>{categoria.titulo}</li>;
+        })}
+      </ul>
 
       <Link to="/">Ir para Home</Link>
     </PageDefault>
